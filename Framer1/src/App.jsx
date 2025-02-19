254903d72528853ce10efe417d432d564310bd21
Arun Kumar <stockerunknown@gmail.com>
2025-02-19 23:06:15 +0530

    Learning new animations in Framer Motion

--- Framer1/src/App.jsx
+++ Framer1/src/App.jsx
@@ -6,7 +6,11 @@ import { motion } from 'motion/react';
 const App = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
+  <motion.div className='w-[200px] h-[200px] bg-[crimson] border-solid border-4 border-white' >
+
+
     
+  </motion.div>
     </div>
   )
 }
